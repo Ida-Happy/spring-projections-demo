ff6b66f5e61c60eb755ce265a9fa3585eab5c2f3
iyuan <hang.yuan@rakuten.com>
2023-10-30 13:45:39 +0800

    remove useless files

---
+++ src/main/java/com/example/demo/SpringProjectionsDemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import com.example.demo.domain.DemoItem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.data.relational.core.mapping.event.BeforeSaveEvent;
+
+@SpringBootApplication
+public class SpringProjectionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectionsDemoApplication.class, args);
+	}
+
+	@Bean
+	public DefaultConversionService conversionService() {
+		return new DefaultConversionService();
+	}
+
+	@EventListener
+	public void handleBeforeSaveEvent(BeforeSaveEvent<?> event) {
+		Class<?> entityClass = event.getEntity().getClass();
+		if (entityClass.equals(DemoItem.class)) {
+			DemoItem orderItem = (DemoItem) event.getEntity();
+			if (orderItem.getId() == null) {
+				orderItem.setId(4);
+			}
+		}
+	}
+}
